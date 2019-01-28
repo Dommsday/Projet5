f1c82ad53d18e9dd4657bc840ef9a529e86da606
Dommsday <Julienzimmermann@msn.com>
2019-01-28 23:55:14 +0100

    Ajout de fonctionnalité à l'inventaire

--- Autoload/Web/js/playerStorage.js
+++ Autoload/Web/js/playerStorage.js
@@ -5,8 +5,9 @@ var PlayerStorage = {
 	newGame: document.getElementById("new-game"),
 	lifePlayer: document.getElementById("life_players"),
 	damagePlayer: document.getElementById("damages_players"),
+	weaponPlayer: document.getElementById("weapon_players"),
 	btnInventory : document.getElementById("button_inventory"),
-	//btnObject: document.getElementById("btnObject"),
+	btnAttakEnemy: document.getElementsByClassName("button-attak-enemy"),//Bouton d'attaque
 
 	btnInput : document.querySelector('input[type="submit"]'),
 
@@ -25,7 +26,10 @@ var PlayerStorage = {
 
 	statLife: document.getElementById("statLife"),
 	statDamage: document.getElementById("statDamage"),
+	nameWeapon: document.getElementById("nameWeapon"),
+	statLifetime: document.getElementById("statLifetime"),
 
+	textName: document.createElement("p"),
 	textDescription: document.createElement("p"),
 	textDamages: document.createElement("p"),
 	textLife: document.createElement("p"),
@@ -36,25 +40,24 @@ var PlayerStorage = {
 	init: function(){
 
 		PlayerStorage.refresh();
+		
+		PlayerStorage.hidden_all_modal();
+		PlayerStorage.show_modal_description();
+		PlayerStorage.attakEnemy();
+
 		//Partie vie du joueur
 		let playerLife = PlayerStorage.lifePlayer.textContent;
-		
 		PlayerStorage.storagePlayer.setItem("lifePlayer", playerLife);
-
-		//PlayerStorage.lifeStorage = PlayerStorage.storagePlayer.getItem("lifePlayer");
-		//alert("La MEMOIRE VIE est de "+PlayerStorage.lifeStorage);
 
 		//Partie dégâts du joueur
 		let damagePlayer = PlayerStorage.damagePlayer.textContent;
-
 		PlayerStorage.storagePlayer.setItem("damagePlayer", damagePlayer);
-		//PlayerStorage.damageStorage = PlayerStorage.storagePlayer.getItem("damagePlayer");
 
-		//PlayerStorage.btnObject.disabled = true;
+		//Arme du joueur
+		let weaponPlayer = PlayerStorage.weaponPlayer.textContent;
+		PlayerStorage.storagePlayer.setItem("weaponPlayer", weaponPlayer);
+
 		PlayerStorage.all_modal_description.style.display="none";
-
-		this.hidden_all_modal();
-		this.show_modal_description();
 
 		PlayerStorage.deconnexion.addEventListener("click", function(){
 			PlayerStorage.storagePlayer.clear();
@@ -100,16 +103,26 @@ var PlayerStorage = {
 
 	show_modal_description: function(){
 
-		PlayerStorage.statLife.textContent = PlayerStorage.storagePlayer.getItem("lifePlayer");
-		PlayerStorage.statDamage.textContent = PlayerStorage.storagePlayer.getItem("damagePlayer");
+		PlayerStorage.statLife.textContent = PlayerStorage.lifePlayer.textContent;
+		PlayerStorage.statDamage.textContent = PlayerStorage.damagePlayer.textContent;
 
 		for(let i = 0; i< PlayerStorage.modal_object.length; i++){
 
-
-
-
 			//QUAND ON SURVOLE LES DIFFERENTS OBJETS DE L'INVENTAIRE
 			PlayerStorage.modal_object[i].addEventListener("mouseover",function(){
+
+				for(let h = 0; h < PlayerStorage.modal_object.length; h++){
+
+					if(i == h){
+						
+						//AFFICHE LE NOM 
+						PlayerStorage.textName.textContent = "Nom : "+PlayerStorage.modal_object[h].textContent;
+
+						PlayerStorage.all_modal_description.appendChild(PlayerStorage.textName);
+						PlayerStorage.all_modal_description.style.display="block";
+
+					}
+				}
 
 				for(let j = 0; j < PlayerStorage.modal_description.length; j++){
 
@@ -186,6 +199,7 @@ var PlayerStorage = {
 						PlayerStorage.btnObject.setAttribute("type", "button");
 						PlayerStorage.btnObject.setAttribute("class", "btnModal");
 						PlayerStorage.btnObject.appendChild(PlayerStorage.modal_href[i]);
+
 						//AFFICHE LE BOUTON D'UTILISATION
 						PlayerStorage.all_modal_objects.appendChild(PlayerStorage.btnObject);
 						PlayerStorage.btnObject.style.display="block";
@@ -197,12 +211,24 @@ var PlayerStorage = {
 				PlayerStorage.storagePlayer.setItem("objectLife", PlayerStorage.modal_life[i].textContent);
 				PlayerStorage.storagePlayer.setItem("objectDamages", PlayerStorage.modal_damages[i].textContent);
 				PlayerStorage.storagePlayer.setItem("objectName", PlayerStorage.modal_object[i].textContent);
+				PlayerStorage.storagePlayer.setItem("objectLifetime", PlayerStorage.modal_lifetime[i].textContent);
 				
 				
 				PlayerStorage.spanLife.textContent = "";
 				PlayerStorage.spanDamages.textContent = "";
+				PlayerStorage.statLifetime.textContent = "";
+
 				PlayerStorage.spanLife.textContent += " + "+PlayerStorage.storagePlayer.getItem("objectLife");
 				PlayerStorage.spanDamages.textContent += " + "+PlayerStorage.storagePlayer.getItem("objectDamages");
+
+				if(!(PlayerStorage.storagePlayer.getItem("objectLifetime") == 1)){
+					PlayerStorage.nameWeapon.textContent = PlayerStorage.storagePlayer.getItem("objectName");
+				}else{
+					PlayerStorage.nameWeapon.textContent = "Mains nues";
+				}
+
+				PlayerStorage.statLifetime.textContent = PlayerStorage.storagePlayer.getItem("objectLifetime");
+				
 
 			});
 		}
@@ -219,27 +245,90 @@ var PlayerStorage = {
 			PlayerStorage.spanLife.textContent = "";
 			PlayerStorage.spanDamages.textContent = "";
 
+			let newNameWeapon = PlayerStorage.storagePlayer.getItem("objectName");
 			let newStatLife = Number(PlayerStorage.storagePlayer.getItem("objectLife")) + Number(PlayerStorage.storagePlayer.getItem('lifePlayer'));
 			let newStatDamage = Number(PlayerStorage.storagePlayer.getItem("objectDamages")) + Number(PlayerStorage.storagePlayer.getItem('damagePlayer'));
+			let newStatLifetime = PlayerStorage.storagePlayer.getItem("objectLifetime");
 
+			//VALEUR DANS LA BARRE DU JEU
 			PlayerStorage.lifePlayer.textContent = newStatLife;
 			PlayerStorage.damagePlayer.textContent = newStatDamage;
+			PlayerStorage.weaponPlayer.textContent = newNameWeapon;
+
+			//VALEUR DANS L'INVENTAIRE
 			PlayerStorage.statLife.textContent = newStatLife;
 			PlayerStorage.statDamage.textContent = newStatDamage;
 
+			if(!(PlayerStorage.storagePlayer.getItem("objectLifetime") == 1)){
 
+				PlayerStorage.statLifetime.textContent = newStatLifetime;
+				PlayerStorage.nameWeapon.textContent = newNameWeapon;
+			}
+
+
+			PlayerStorage.storagePlayer.setItem("objectName", PlayerStorage.weaponPlayer.textContent);
 			PlayerStorage.storagePlayer.setItem("lifePlayer", PlayerStorage.lifePlayer.textContent);
 			PlayerStorage.storagePlayer.setItem("damagePlayer", PlayerStorage.damagePlayer.textContent);
-					
+			PlayerStorage.storagePlayer.setItem("objectLifetime", PlayerStorage.statLifetime.textContent);
+
 		});
 	},
 
 	refresh: function(){
 
 		if(PlayerStorage.storagePlayer.length > 0){
-			
+
 			PlayerStorage.lifePlayer.textContent = PlayerStorage.storagePlayer.getItem("lifePlayer");
 			PlayerStorage.damagePlayer.textContent = PlayerStorage.storagePlayer.getItem("damagePlayer");
+
+			if(PlayerStorage.storagePlayer.getItem("objectLifetime") >= 1){
+			PlayerStorage.statLifetime.textContent = PlayerStorage.storagePlayer.getItem("objectLifetime");
+
+			//BARRE DU JEU
+			PlayerStorage.weaponPlayer.textContent = PlayerStorage.storagePlayer.getItem("objectName");
+			//DANS L'INVENTAIRE
+			PlayerStorage.nameWeapon.textContent = PlayerStorage.storagePlayer.getItem("objectName");
+			}
+		}
+	},
+
+	attakEnemy: function(){
+
+		for(let i = 0; i < PlayerStorage.btnAttakEnemy.length; i++){
+
+			PlayerStorage.btnAttakEnemy[i].addEventListener("click", function(){
+
+				if(PlayerStorage.statLifetime.textContent !== "/"){
+
+				let newStatLifetime = Number(PlayerStorage.storagePlayer.getItem("objectLifetime")) - 1;
+
+				//VALEUR DANS L'INVENTAIRE
+				PlayerStorage.statLifetime.textContent = newStatLifetime;
+
+				PlayerStorage.storagePlayer.setItem("objectLifetime", PlayerStorage.statLifetime.textContent);
+
+				if(PlayerStorage.statLifetime.textContent <= 0){
+
+					alert("!!! Arme brisée !!!!!")
+
+					PlayerStorage.statLifetime.textContent = "/";
+					PlayerStorage.weaponPlayer.textContent = "Mains nues";
+					PlayerStorage.nameWeapon.textContent = "Mains nues";
+
+					//On remet la force initiale
+					let restartDamages = PlayerStorage.storagePlayer.getItem('damagePlayer') - PlayerStorage.storagePlayer.getItem("objectDamages");
+
+
+					PlayerStorage.damagePlayer.textContent = restartDamages;
+					PlayerStorage.statDamage.textContent = restartDamages;
+
+					PlayerStorage.storagePlayer.setItem('damagePlayer', PlayerStorage.damagePlayer.textContent)
+					PlayerStorage.storagePlayer.setItem("objectDamages", PlayerStorage.statDamage.textContent);
+				}
+
+			}
+
+			});
 		}
 	}
 }
